50c586c442450b1db0d6c598e4ea48d2f629a601
Daniel Pereira <Daniel.Pereira@cisbaf.local>
2022-05-27 11:08:17 -0300

    deploy

--- blog/settings.py
+++ blog/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'templates/static'
 ]
+STATIC_ROOT = BASE_DIR / 'static'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 
@@ -146,3 +148,14 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
+try:
+    from.local_settings import *
+except:
+    pass
